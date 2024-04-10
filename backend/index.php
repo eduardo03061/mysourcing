74c22b772958b6624bb60a762c509f220161e9a4
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-04-10 09:43:36 -0600

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,5 @@
 <?php
-
-
-
+ 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Aceppt, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
