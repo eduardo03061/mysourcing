6f28be8abeb1316afc7c66ff8a3c63e8e36f8a45
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-04-09 18:38:10 -0600

    feat: front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
+const routes: Routes = [
+  { path: 'users', component: UserComponent },
+];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent, RouterLink, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'users',component: UserComponent
+    },
+];
