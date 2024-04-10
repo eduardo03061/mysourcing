dc78a8739a1ad6a42d4a5a451b16faf3fb359a3f
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-04-10 08:17:54 -0600

    Update userList.component.ts

--- front/src/app/user/user-list/userList.component.ts
+++ front/src/app/user/user-list/userList.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../core/services/data.service';
+import { EMPTY, Observable, catchError } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './userList.component.html',
   styleUrl: './userList.component.css'
 })
 export class UserListComponent {
-  username ="eduardo";
-  isLoggin = true;
+
+  public usersResults$!: Observable<any>;
+  public errorMessage!: string;
+  constructor(private dataService: DataService) { }
+
+
+
+  ngOnInit() {
+    this.dataService.getUsers(1).subscribe(resp => {
+       
+      
+      resp = JSON.parse(resp);
+      console.log('resp',resp);
+      this.usersResults$ = resp;
+  });
+    console.log('results',this.usersResults$)
+  }
+
+
+
 }
